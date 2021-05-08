2c3599a9e3ddb09e27d08164e8052203d01cdfb0
Andre Almeida <andre_rdo@hotmail.com>
2021-05-08 14:50:20 -0300

    first commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,11 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 3
+    @State private var tipPercentage = [0,5,10,15,20,25,30]
+    @State private var tipSelection: Double = 10
+    @State private var isEditing = false
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+  
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{
+            
+            Form{
+                
+                Section(header: Text("Insira do valor aqui:")){
+                    TextField("Valor final", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("Quantas pessoas vão dividir a conta?")){
+                    Picker("Escolha o número de pessoas", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) Pessoas")
+                        }
+                    }
+                }
+                
+                Section(header: Text("Valor da gorjeta:")){
+                    Slider(
+                        value: $tipSelection,
+                        in: 0...30,
+                        step: 5,
+                        onEditingChanged: { editing in
+                            isEditing = editing
+                        },
+                        minimumValueLabel: Text("0%"),
+                        maximumValueLabel: Text("30%")
+                    ) {
+                        Text("Gorjeta")
+                    }
+                    Text("\(Int(tipSelection))%")
+                        .foregroundColor(isEditing ? .red : .blue)
+                }
+                
+                Section(header: Text("Total por pessoa:")){
+                    Text("R$ \(totalPerPerson, specifier: "%.2f")")
+                        .font(.largeTitle)
+                }
+                
+            }
+            .navigationTitle("WeSplit")
+            
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
